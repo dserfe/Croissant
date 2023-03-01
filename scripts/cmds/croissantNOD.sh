43bd10124d571e5f16b8c2f755f84df7abd96a25
deserfe <123439795+dserfe@users.noreply.github.com>
2023-02-28 23:40:49 -0600

    update scripts

--- scripts/cmds/croissantNOD.sh
+++ scripts/cmds/croissantNOD.sh
@@ -3,7 +3,7 @@ testClass=$2
 junit=$3
 
 if [ $junit == '4' ]; then
-    timeout 1200s mvn exec:java -Dexec.mainClass=com.framework.App -Dexec.args="-dir ${module}/test-classes -o ${module}/test-classes -t 1 -n ${testClass}"
+    timeout 1200s mvn exec:java -Dexec.mainClass=com.framework.App -Dexec.args="-dir ${module}/test-classes -o ${module}/test-classes -t 1 -n ${testClass} -all_nod_id"
 else
-    timeout 1200s mvn exec:java -Dexec.mainClass=com.framework.App -Dexec.args="-dir ${module}/test-classes -o ${module}/test-classes -t 1 -n ${testClass} -j"
+    timeout 1200s mvn exec:java -Dexec.mainClass=com.framework.App -Dexec.args="-dir ${module}/test-classes -o ${module}/test-classes -t 1 -n ${testClass} -all_nod_id -j"
 fi
